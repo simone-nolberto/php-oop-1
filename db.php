f659a7e05883064bb68bcecc5b0876ef24079932
simone-nolberto <simone.nolberto@outlook.com>
2024-04-19 17:52:10 +0200

    update with minor fix

--- db.php
+++ db.php
@@ -6,16 +6,18 @@ require_once __DIR__ . '/models/TvSerie.php';
 
 
 $productions = [
-    new Production('Benvenuti al sud', 'italian', 6, new Genre('movie', 'a classical italian comedy')),
-    new Production('Non sposate le mie figlie', 'french', 7, new Genre('movie', 'a classical french comedy')),
+    new Production('Benvenuti al sud', 'italian', 6, new Genre('comedy', 'a classical italian comedy')),
+    new Production('Non sposate le mie figlie', 'french', 7, new Genre('comedy', 'a classical french comedy')),
     
     
 ];
 
 $TvSeries = [
-    new TvSerie('How i met your mother', 'en', 9, new Genre('show', 'a classical american sitcom'), 9),
+    new TvSerie('How i met your mother', 'en', 9, new Genre('sitcom', 'a classical american sitcom'), 9),
+    new TvSerie('New Girl', 'en', 8, new Genre('show', 'a story about 3 broke guys and a silly girl'), 7 )
 ];
 
 $Movies = [
-    new Movie('Avatar', 'en', 8, new Genre('movie', 'a story about the conquer of an alien planet'), 100, 3),
+    new Movie('Avatar', 'en', 8, new Genre('action', 'a story about the conquer of an alien planet'), 100, 3),
+    new Movie('Matrix','en', 7, new Genre('sci-fi', 'the story of a common man who defeats evil force'), 300, 2-5),
 ];
--- index.php
+++ index.php
@@ -20,61 +20,72 @@ require_once __DIR__ . '/db.php';
     <h1>Hello World</h1>
 
     <div class="container">
+        <h3>Produzioni: </h3>
         <div class="row">
             <?php foreach ($productions as $production) : ?>
-
                 <div class="col">
                     <div class="card">
 
                         <div class="card-body">
-                            <h4 class="card-title"><?php echo $production->getTitle() ?></h4>
-                            <p class="card-language">Lingua: <?php echo $production->getLanguage() ?></p>
-                            <p class="card-language">Voto: <?php echo $production->getVote() ?></p>
-                            <p class="card-language">Genere: <?php echo $production->getGenre() ?></p>
+                            <h4 class="card-title"><?php echo $production->title ?></h4>
+                            <p>Lingua: <?php echo $production->language ?></p>
+                            <p>Voto: <?php echo $production->vote ?></p>
+                            <p>Genere: <?php echo $production->genre->name ?></p>
+                            <p>Genere: <?php echo $production->genre->description ?></p>
+
                         </div>
 
                     </div>
                 </div>
-
             <?php endforeach ?>
 
+        </div>
+
+
+        <h3>Show: </h3>
+        <div class="row">
             <?php foreach ($TvSeries as $TvSerie) : ?>
 
                 <div class="col">
                     <div class="card">
 
                         <div class="card-body">
-                            <h4 class="card-title"><?php echo $TvSerie->getTitle() ?></h4>
-                            <p class="card-language">Lingua: <?php echo $TvSerie->getLanguage() ?></p>
-                            <p class="card-language">Voto: <?php echo $TvSerie->getVote() ?></p>
-                            <p class="card-language">Genere: <?php echo $TvSerie->getGenre() ?></p>
-                            <p>Stagioni: <?php $TvSerie->getSeasonNumber() ?> </p>
+                            <h4 class="card-title"><?php echo $TvSerie->title ?></h4>
+                            <p>Lingua: <?php echo $TvSerie->language ?></p>
+                            <p>Voto: <?php echo $TvSerie->vote ?></p>
+                            <p>Genere: <?php echo $TvSerie->genre->name ?></p>
+                            <p>Genere: <?php echo $TvSerie->genre->description ?></p>
+                            <p>Stagioni: <?php $TvSerie->seasonNumber ?> </p>
                         </div>
 
                     </div>
                 </div>
-
             <?php endforeach ?>
+        </div>
 
+
+        <h3>Film: </h3>
+        <div class="row">
             <?php foreach ($Movies as $Movie) : ?>
 
                 <div class="col">
                     <div class="card">
 
                         <div class="card-body">
-                            <h4 class="card-title"><?php echo $Movie->getTitle() ?></h4>
-                            <p class="card-language">Lingua: <?php echo $Movie->getLanguage() ?></p>
-                            <p class="card-language">Voto: <?php echo $Movie->getVote() ?></p>
-                            <p class="card-language">Genere: <?php echo $Movie->getGenre() ?></p>
-                            <p>Introiti: <?php $Movie->getProfits() ?></p>
-                            <p>Durata: <?php $Movie->getLasting() ?></p>
+                            <h4 class="card-title"><?php echo $Movie->title ?></h4>
+                            <p>Lingua: <?php echo $Movie->language ?></p>
+                            <p>Voto: <?php echo $Movie->vote ?></p>
+                            <p>Genere: <?php echo $Movie->genre->name ?></p>
+                            <p>Genere: <?php echo $Movie->genre->description ?></p>
+                            <p>Introiti: <?php $Movie->profits ?></p>
+                            <p>Durata: <?php $Movie->lasting ?></p>
                         </div>
 
                     </div>
                 </div>
-
             <?php endforeach ?>
         </div>
+
     </div>
 
 
